d6f0d3ed67d566095f0e504a4e847bd0a19c2cc9
xmarkgs <mraner123@gmail.com>
2021-01-19 20:44:00 +0200

    Deleted template

---
+++ src/containers/search-app.jsx
@@ -0,0 +1,13 @@
+import { React, Component, Fragment } from 'react';
+
+class SearchApp extends Component {
+    render(){
+        return (
+            <Fragment>
+                <h1>Hello World!</h1>
+            </Fragment>
+        )
+    }
+}
+
+export default SearchApp;
